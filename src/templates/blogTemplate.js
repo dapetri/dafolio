6ebd2441c66b59c6ba783b86bff98b38b4b0f7e6
dapetri <dapetri0@gmail.com>
2020-03-21 17:48:28 +0100

    working on template

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,24 +1,112 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from "react";
+import styled from "styled-components";
+import tw from "tailwind.macro";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { kebabCase } from "lodash";
+import { Link, graphql } from "gatsby";
+import "./all.sass";
 
-export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
-}) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark
+// Components
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/PostContent";
+
+export const BlogPostTemplate = ({
+  content,
+  contentComponent,
+  description,
+  tags,
+  title,
+  helmet
+}) => {
+  const PostContent = contentComponent || Content;
+
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+    <section className="section">
+      {helmet || ""}
+      <div className="container content">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+              {title}
+            </h1>
+            <p>{description}</p>
+            <div style={{ marginBottom: `1rem` }}>
+              <Link to="/blog">{`< Back to list`}</Link>
+            </div>
+            <PostContent content={content} />
+            {tags && tags.length ? (
+              <div style={{ marginTop: `4rem` }}>
+                <h4>Tags</h4>
+                <ul className="taglist">
+                  {tags.map(tag => (
+                    <li key={tag + `tag`}>
+                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
+            <div style={{ marginTop: `4rem` }}>
+              <Link to="/blog">{`< Back to list`}</Link>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  )
-}
+    </section>
+  );
+};
+
+BlogPostTemplate.propTypes = {
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  helmet: PropTypes.object
+};
+
+const ContentBackground = styled.div`
+  ${tw`fixed w-full h-full`};
+  background: #fff;
+  clip-path: polygon(0 1%, 100% 2%, 100% 99%, 0 98%);
+`;
+
+const BlogPost = ({ data }) => {
+  const { markdownRemark: post } = data;
+
+  return (
+    <>
+      <Layout />
+      <>
+        <ContentBackground />
+        <BlogPostTemplate
+          content={post.html}
+          contentComponent={HTMLContent}
+          description={post.frontmatter.description}
+          helmet={
+            <Helmet titleTemplate="%s | Blog">
+              <title>{`${post.frontmatter.title}`}</title>
+              <meta
+                name="description"
+                content={`${post.frontmatter.description}`}
+              />
+            </Helmet>
+          }
+          tags={post.frontmatter.tags}
+          title={post.frontmatter.title}
+        />
+      </>
+    </>
+  );
+};
+
+BlogPost.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object
+  })
+};
+
+export default BlogPost;
 
 export const pageQuery = graphql`
   query($path: String!) {
@@ -28,7 +116,9 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         path
         title
+        description
+        # tags # TODO: add tag listing functionality
       }
     }
   }
-`
+`;
