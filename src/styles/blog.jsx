1f5c34522aac721c4636885730531f313a4eea51
dapetri <dapetri0@gmail.com>
2020-03-22 18:53:32 +0100

    working on template (#6)

--- src/styles/blog.jsx
+++ src/styles/blog.jsx
@@ -1,1 +1,36 @@
+import styled from "styled-components";
 
+export const Background = styled.div`
+  position: fixed;
+  padding: 0;
+  margin: 0;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgb(21, 24, 32);
+`;
+
+export const SnippetBackground = styled.div`
+  position: fixed;
+  padding: 0;
+  margin: 0;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #fff;
+  clip-path: polygon(
+    12% 2%,
+    28% 0%,
+    37% 2%,
+    79% 0%,
+    100% 5%,
+    100% 95%,
+    95% 100%,
+    75% 97%,
+    29% 100%,
+    0% 95%,
+    0% 0%
+  );
+`;
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,21 +1,27 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { Background, SnippetBackground } from "../styles/blog";
 
 // data prop will be injected by the GraphQL query below.
 export default function Template({ data }) {
   const { markdownRemark } = data; // data.markdownRemark is property of data that holds all of post data
   const { frontmatter, html } = markdownRemark;
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-    </div>
+    <Background>
+      <SnippetBackground>
+        <section className="section"></section>
+        <div className="blog-post-container">
+          <div className="blog-post">
+            <h1>{frontmatter.title}</h1>
+            <h2>{frontmatter.date}</h2>
+            <div
+              className="blog-post-content"
+              dangerouslySetInnerHTML={{ __html: html }}
+            />
+          </div>
+        </div>
+      </SnippetBackground>
+    </Background>
   );
 }
 
