beb87c2d255587d0e705dc506d63f5a006ee546c
dapetri <dapetri0@gmail.com>
2020-03-22 22:11:36 +0100

    working on template (#7)

--- src/styles/blog.jsx
+++ src/styles/blog.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import "typeface-open-sans";
 
 export const Background = styled.div`
   position: fixed;
@@ -33,4 +34,29 @@ export const SnippetBackground = styled.div`
     0% 95%,
     0% 0%
   );
+`;
+
+export const TextBody = styled.html`
+  text-rendering: optimizeLegibility;
+  overflow-x: hidden;
+  box-sizing: border-box;
+  -ms-overflow-style: scrollbar;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  background-color: #161719;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+`;
+
+export const BigTitle = styled.h1`
+  ${tw`text-5xl lg:text-6xl font-serif text-white mb-6 tracking-wide`};
+  text-shadow: 0 5px 35px rgba(255, 255, 255, 0.15);
+`;
+
+export const Subtitle = styled.p`
+  ${tw`text-2xl lg:text-4xl font-sans text-white mt-8 xxl:w-3/4`};
+  text-shadow: 0 2px 15px rgba(0, 0, 0, 0.2);
 `;
