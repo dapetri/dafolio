16ffbf376126bb9cabad4d59e48d84caafb00c9e
dapetri <dapetri0@gmail.com>
2020-04-01 12:19:04 +0200

    first release (#1.22)

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   pathPrefix: config.pathPrefix,
   siteMetadata: {
     // Will be used to generate absolute URLs for og:image etc.
+    siteTitleAlt: config.siteTitle,
     siteUrl: config.siteUrl + pathPrefix
   },
 
