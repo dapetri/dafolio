a0acc47d519584638eda62ea337107418d76f24a
dapetri <me@dapetri.com>
2024-01-21 00:08:46 +0100

    finishing where was

--- src/Map.js
+++ src/Map.js
@@ -2,20 +2,19 @@ import React, { useRef, useEffect, useState } from 'react'
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import styled from "styled-components";
 import AmongUs from './components/particles/AmongUs';
+import img from "./img/fine_dots_1_by_screentone_dnloi8-pre.jpg"
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGFwZXRyaSIsImEiOiJjbGxyd2JiOHQwOXphM2pvaHFuM2thMDY2In0.z70kqY7Uv2rcnKvzJClqUQ';
 
 export const Map = () => {
     const mapContainer = useRef(null);
-    const map = useRef(null);
-    // const [lng, setLng] = useState(-70.9);
-    // const [lat, setLat] = useState(42.35);
-    // const [zoom, setZoom] = useState(9);
-
+    var map = useRef(null);
+    
     const [lng,] = useState(8.4037);
     const [lat,] = useState(49.0069);
     const [zoom,] = useState(1.9);
-
+    const [countriesVisited, ] = useState(["DEU",'NLD','ITA',"FRA","ISL", "ESP", "LKA", "AUT", "GBR", "AND", "CHE", "HUN", "ROU", "GRC", "EGY", "MAR", "TUR", "ISR", "SVN", "HRV", "TUN", "USA", "MEX", "CUB", "DOM"])
+            
     useEffect(() => {
         if (map.current) return; // initialize map only once
         map.current = new mapboxgl.Map({
@@ -24,7 +23,62 @@ export const Map = () => {
             center: [lng, lat],
             zoom: zoom
         });
+
+        map.current.loadImage(
+           img,
+            (err, image) => {
+                if (err) throw err;
+                map.current.addImage('pattern', image);
+        map.current.on('load', function() {
+
+            map.current.addLayer(
+            {
+                id: 'country-boundaries',
+                source: {
+                type: 'vector',
+                url: 'mapbox://mapbox.country-boundaries-v1',
+                },
+                'source-layer': 'country_boundaries',
+                'disputed': "false",
+                type: 'fill',
+                paint: {
+                "fill-pattern": "pattern",
+                'fill-opacity': 0.1,
+
+                },
+            },
+            'country-label'
+            );
+            
+            map.current.setFilter('country-boundaries', [
+                "all",
+                [
+                  "==",
+                  ["get", "disputed"],
+                  "false"
+                ],
+                [
+                  "any",
+                  [
+                    "==",
+                    "all",
+                    ["get", "worldview"]
+                  ],
+                  [
+                    "in",
+                    "US",
+                    ["get", "worldview"]
+                  ],
+
+                ],
+                ["match", ["get", "iso_3166_1_alpha_3"], countriesVisited, false, true]
+              ]);
+            }
+        )
+
+        });
     });
+
 
     return (
         <>
